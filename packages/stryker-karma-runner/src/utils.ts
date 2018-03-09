2eac86033293f572d3ad77b2e733687fa247cbd2
Nico Jansen <jansennico@gmail.com>
2018-03-09 17:36:13 +0100

    feat(karma-runner): configure files with karma
    * Remove old logic that altered the `files`  array from stryker config
    * Allow files to be configured in karma config

--- packages/stryker-karma-runner/src/utils.ts
+++ packages/stryker-karma-runner/src/utils.ts
@@ -1,3 +1,18 @@
+import * as fs from 'fs';
+
 export function requireModule(name: string): any {
   return require(name);
+}
+
+/**
+ * Creates an empty file if it did not exist
+ * @param fileName the name of the file
+ */
+export function touchSync(fileName: string) {
+  try {
+    const handle = fs.openSync(fileName, 'wx');
+    fs.closeSync(handle);
+  } catch {
+
+  }
 }
