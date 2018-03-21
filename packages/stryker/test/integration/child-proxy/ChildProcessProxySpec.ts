a6a85ac7894c01e41828a3cebc0713670bccd8c7
Nico Jansen <jansennico@gmail.com>
2018-03-21 18:25:10 +0100

    test(integration-test): fix hanging test

--- packages/stryker/test/integration/child-proxy/ChildProcessProxySpec.ts
+++ packages/stryker/test/integration/child-proxy/ChildProcessProxySpec.ts
@@ -9,25 +9,25 @@ describe('ChildProcessProxy', () => {
 
   beforeEach(() => {
     sut = ChildProcessProxy.create(require.resolve('./Echo'), 'info', [], Echo, 'World');
+  });
 
+  afterEach(() => {
+    sut.dispose();
   });
 
   it('should be able to get direct result', async () => {
     const actual = await sut.proxy.say('hello');
     expect(actual).eq('World: hello');
-    sut.dispose();
   });
 
   it('should be able to get delayed result', async () => {
     const actual = await sut.proxy.sayDelayed('hello', 2);
     expect(actual).eq('World: hello (2 ms)');
-    sut.dispose();
   });
 
   it('should be able to receive files', async () => {
     const actual: string = await sut.proxy.echoFile(new File('hello.txt', 'hello world from file'));
     expect(actual).eq('hello world from file');
-    sut.dispose();
   });
 
   it('should be able to send files', async () => {
